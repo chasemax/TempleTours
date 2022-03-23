359460447ca0b205a1067cba79e315dce09ba7d4
JacksonSchow <80366532+JacksonSchow@users.noreply.github.com>
2022-03-23 10:46:22 -0600

    Validation

--- Models/Signup.cs
+++ Models/Signup.cs
@@ -11,16 +11,19 @@ namespace TempleTours.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required (ErrorMessage = "Group Names must be shorter than 50 characters")]
+        [MaxLength(50)]
         public string GroupName { get; set; }
 
-        [Required]
-        [Range(0, 15)]
+        [Required (ErrorMessage = "Group Sizes must be between 1 and 15 persons")]
+        [Range(1, 15)]
         public int GroupSize { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Enter a valid email")]
+        [MaxLength(50)]
         public string Email { get; set; }
 
+        [MaxLength(10)]
         public string Phone { get; set; }
 
         public int AppointmentId { get; set; }
